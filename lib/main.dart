722fe97ff3f13b7303199ca0376d612af255b951
yaseeeeeeeeeeen <muhammardyaseen@gmail.com>
2025-02-27 22:53:03 +0530

    folder_stracture_added

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:dating_app_test/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen(
+    );
+  }
+}
