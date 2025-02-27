6ed50a753f9bb44f3118f2bb923008fcce7e8b49
yaseeeeeeeeeeen <muhammardyaseen@gmail.com>
2025-02-28 04:17:05 +0530

    Completed

--- lib/presentation/widgets/chat_tile.dart
+++ lib/presentation/widgets/chat_tile.dart
@@ -1,7 +1,6 @@
 import 'package:dating_app_test/presentation/cubit/chat_cubit.dart';
 import 'package:flutter/material.dart';
 
-
 class ChatTile extends StatelessWidget {
   final ChatUser user;
 
@@ -53,6 +52,26 @@ class ChatTile extends StatelessWidget {
           if (user.hasUnreadMessage)
             Icon(Icons.circle, color: Colors.orange, size: 10),
         ],
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MockPage()),
+        );
+      },
+    );
+  }
+}
+
+class MockPage extends StatelessWidget {
+  const MockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Chat screen",
+        style: Theme.of(context).textTheme.displayLarge,
       ),
     );
   }
